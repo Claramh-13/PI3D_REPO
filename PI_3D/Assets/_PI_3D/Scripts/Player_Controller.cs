f872bf59a42624831259d77561325993dccb0a3d
Vleriia <valeriaballmar@gmail.com>
2026-05-18 14:15:54 +0200

    Counter

--- PI_3D/Assets/_PI_3D/Scripts/Player_Controller.cs
+++ PI_3D/Assets/_PI_3D/Scripts/Player_Controller.cs
@@ -1,28 +1,40 @@
 using UnityEngine;
-
 public class Character_Controller : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     private bool isWalking;
-
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(-inputVector.x, 0f, -inputVector.y);
-
-        float playerSize = .7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
-
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float playerSize = .3f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDir);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized; 
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirX);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerSize, moveDirZ);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
         if (canMove)
         {
             transform.position += moveDir * moveSpeed * Time.deltaTime;
-
         }
-       
-
         isWalking = moveDir != Vector3.zero;
-
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
