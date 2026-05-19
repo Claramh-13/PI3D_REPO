3532df3f8766497b718d8b95abe9357d922a989e
Vleriia <valeriaballmar@gmail.com>
2026-05-19 13:44:29 +0200

    Calcetines en la encimera

--- PI_3D/Assets/_PI_3D/Scripts/ClearCounter.cs
+++ PI_3D/Assets/_PI_3D/Scripts/ClearCounter.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+   [SerializeField] private Transform CalcetinesPrefab;
+   [SerializeField] private Transform counterTopPoint;
     public void Interact() 
     {
         Debug.Log("interact");
+        Transform CalcetinesTransform = Instantiate(CalcetinesPrefab, counterTopPoint);
+        CalcetinesTransform.localPosition = Vector3.zero;
     }
 }
